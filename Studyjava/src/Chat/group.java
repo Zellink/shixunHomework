ccfeeb6c2042cb22fed9878335836fc3009425fe
ktj57 <870399464@qq.com>
2019-11-25 16:18:18 +0800

    改动

---
+++ Studyjava/src/Chat/group.java
@@ -0,0 +1,8 @@
+package Chat;
+
+public class group {
+	int port;
+	String password;
+	String groupowner;
+	String groupname;
+}
--- Studyjava/src/interface_/Collection_test.java
+++
@@ -1,5 +0,0 @@
-package interface_;
-import java.util.*;
-public class Collection_test implements Collection{
-	
-}
