2a43720381bed04accb0d30c51de557aa94f77c7
Zellink <769541931@qq.com>
2019-11-25 22:47:09 +0800

    占个坑

---
+++ Studyjava/src/Chat/ChooseRoomPanel.java
@@ -0,0 +1,17 @@
+package Chat;
+
+import javax.swing.JFrame;
+
+public class ChooseRoomPanel {
+	
+	private JFrame jframe;
+
+	public static void main(String[] args) {
+		new ChooseRoomPanel();
+	}
+	
+	public ChooseRoomPanel() {
+		jframe = new JFrame();
+	}
+
+}
